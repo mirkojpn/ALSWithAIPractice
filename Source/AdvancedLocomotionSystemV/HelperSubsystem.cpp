b51de7ba3157d2237f4a083797de2530f4e1101b
mirkojpn <mirkojpn@gmail.com>
2022-03-18 17:58:22 +0900

    Advanced Locomotion SystemでAI実装（プレイヤーが武器出したらAIも自分の出して撃つ）。

---
+++ Source/AdvancedLocomotionSystemV/HelperSubsystem.cpp
@@ -0,0 +1,6 @@
+
+
+
+#include "HelperSubsystem.h"
+
+
---
+++ Source/AdvancedLocomotionSystemV/HelperSubsystem.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "HelperSubsystem.generated.h"
+
+class APawn;
+/**
+ * 
+ */
+UCLASS()
+class ADVANCEDLOCOMOTIONSYSTEMV_API UHelperSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+};
